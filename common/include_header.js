11c131b594332e5d7becade12ad8ed04634f190f
Baptiste <billard.baptiste.etudes@gmail.com>
2020-06-16 16:17:30 +0200

    header colors and tags

---
+++ common/include_header.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+    <header> \
+		<div id="home-link"><a href="home_page.html">Home</a></div> \
+			<div class="title"> \
+				<h1>Baptiste Billard</h1> \
+				<h2><span class="tag red">Game Designer</span><span class="tag orange">Level Designer</span><span class="tag blue">Gameplay Programmer</span></h2> \
+			</div> \
+		<div id="contact-link"><a href="../contact_page/contact_page.html">Contact &amp; CV</a></div> \
+	</header> \
+\
+');
